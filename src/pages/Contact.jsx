06bf35e5599469188f2b04e9f49a2d7ac32383de
kevinpaul468 <kevinpaul468@gmail.com>
2024-10-08 10:13:46 +0530

    made changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import img1 from "../assets/logo.png";
+// import img1 from "../assets/logo.png";
 import { MdOutlineMail } from "react-icons/md";
 import { MdAddCall } from "react-icons/md";
 import { BsTwitterX } from "react-icons/bs";
@@ -9,9 +9,9 @@ function Contact() {
   return (
     <div id="Contact" className="p-4 min-h-[70%] md:p-8">
       <div className="flex flex-col md:flex-row justify-evenly items-center gap-8">
-        <div className="w-full md:w-1/2 flex  m-2 justify-center">
+        {/* <div className="w-full md:w-1/2 flex  m-2 justify-center">
           <img src={img1} alt="Logo" className="max-w-xs md:max-w-sm" />
-        </div>
+        </div> */}
         <div className="w-full md:max-w-1/2 bg-white shadow-lg p-6 rounded-lg">
           <h2 className="text-2xl font-semibold mb-4 text-center md:text-left">
             Ask a Question
@@ -66,20 +66,20 @@ function Contact() {
         <div className="flex-col min-w-[30%] p-2 ">
           <span className=" flex gap-3 items-center justify-center text-xl my-2">
           <MdOutlineMail />
-            <a href="mailto:Mohammedabdulrahman@ieee.org" className="hover:underline"> Mohammedabdulrahman@ieee.org</a>
+            <a href="mailto:ieee_gnitc@gniindia.org" className="hover:underline">ieee_gnitc@gniindia.org</a>
           </span>
           <span className=" flex gap-3 my-2 items-center justify-center text-xl"> 
-          <MdAddCall />
-            <a href="tel:+91 8008243308">+91 8008243308</a> <a href="tel:+91 9951150501">+91 9951150501</a>
+          {/* <MdAddCall /> */}
+            {/* <a href="tel:+91 8008243308">+91 8008243308</a> <a href="tel:+91 9951150501">+91 9951150501</a> */}
           </span>
           
           <span className=" flex gap-3 my-2 items-center justify-center text-xl">
           <BsTwitterX />
-            <a href="https://x.com/IEEE_GNITC">x-twitter</a>
+            {/* <a href="https://x.com/IEEE_GNITC">x-twitter</a> */}
           </span>
           <span className=" flex my-2 gap-3 items-center justify-center text-xl">
           <FaInstagram />
-            <a href="https://www.instagram.com/ieee_gnitc/">Instagram</a>
+            <a href="https://www.instagram.com/ieee_gni/">Instagram</a>
           </span>
           <span className=" my-2 flex gap-3 items-center justify-center text-xl">
           <span className="text-2xl"><TfiLocationPin /></span>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,10 +8,14 @@ function Home() {
         <div className=' md:w-[50%] flex flex-col mx-28 gap-10  '>
             <h1 className='text-[#171717]  mx-4 md:mx-0  text-4xl md:text-5xl font-semibold'>Unleash Your Creativity at Hackarena</h1>
             <h1 className='text-[#626262]  mx-4 md:mx-0  text-xl md:text-2xl '>"Join us to solve real-world challenges, collaborate with innovators, and turn your ideas into reality.turn your ideas into reality."</h1>
+          
+          <a href='https://konfhub.com/hack-arena-ieee-gni'>
           <div className='flex items-center justify-center gap-[15px] w-[280px] h-[55px] rounded-[75px] mt-[20px] bg-[#ff4141] font-medium text-xl text-white hover:scale-105 ease-in-out duration-300'>
             <button>Register Now</button>
+            
             <img src={arrow_img} alt="" className='' />
           </div>
+          </a>
         </div>
         <div className='w-[50%] '>
           <img src={hackimg4} alt="" className='shadow-[10px_-5px_50px_-5px] shadow-blue-200 ' />
