b599ed30124e9cb4fefab140e6752ed5fd7fd6c0
uma santoshi tanuku <111972093+santoshi746@users.noreply.github.com>
2024-10-08 10:28:12 +0530

    Add files via upload

---
+++ src/pages/More.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import './more.css'
+function More() {
+  return (
+    <div id="More">
+      <div class="container">
+        <h1 id="Theme_Title">Theme</h1>
+        <div class="row my-2">
+          <div class="col-12 col-lg-4 mb-lg-12">
+            <div class="card more-card" id="card1">
+              <div class="card-body">
+                <span class=" more-icons bi bi-camera-video-off-fill"></span>
+                <p class="">Security and surveillance</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-lg-4 mb-lg-12">
+            <div class="card more-card" id="card2">
+              <div class="card-body">
+                <span class=" more-icons bi bi-controller"></span>
+                <p class="">Game Development</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-lg-4 mb-lg-12">
+            <div class="card more-card" id="card3">
+              <div class="card-body">
+                <span class=" more-icons bi bi-shield-fill-check"></span>
+                <p class="">Defence</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row my-2">
+          <div class="col-12 col-lg-4 mb-lg-12">
+            <div class="card more-card" id="card4">
+              <div class="card-body">
+                <span class=" more-icons bi bi-lungs-fill"></span>
+                <p class="">Health and Biotech</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-lg-4 mb-lg-12">
+            <div class="card more-card" id="card5">
+              <div class="card-body">
+                <span class=" more-icons bi bi-lightbulb-fill"></span>
+                <p class="">Open innovations</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-lg-4 mb-lg-12">
+            <div class="card more-card" id="card6">
+              <div class="card-body">
+                <span class=" more-icons bi bi-motherboard-fill"></span>
+                <p class="">Generative AI</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row my-2">
+          <div class="col-12 col-lg-4 mb-lg-12">
+            <div class="card more-card" id="card7">
+              <div class="card-body">
+                <span class=" more-icons bi bi-diagram-3-fill"></span>
+                <p class="">Blockchain and cryptocurrency</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-lg-4 mb-lg-12">
+            <div class="card more-card" id="card8">
+              <div class="card-body">
+                <span class=" more-icons bi bi-basket2-fill"></span>
+                <p class="">E-Commerce</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-lg-4 mb-lg-12">
+            <div class="card more-card" id="card9">
+              <div class="card-body">
+                <span class=" more-icons bi bi-bookmark-check-fill"></span>
+                <p class="">Culture and Heritage Travel</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default More
